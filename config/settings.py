c54a47f7fa48f4e86f772ff25da308a09d20f74b
Fabian Lengg <fabian.lengg@gmail.com>
2026-06-11 11:24:02 +0200

    datenschutz und impressum verbesser

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
